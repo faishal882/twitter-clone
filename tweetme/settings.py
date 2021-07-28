891feca86e8e0b951227bacc3a7b723ad13fbd24
Faishal Manzar <faishalmanzar@gmail.com>
2021-07-28 16:38:37 +0530

    Initial commit

--- tweetme/settings.py
+++ tweetme/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    # third-party
+    'rest-framework',
+    # Internal
     'tweets',
 ]
 
