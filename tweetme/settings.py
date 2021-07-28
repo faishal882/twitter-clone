71616336c599666f37314be79b2c0b46992a11b5
Faishal Manzar <faishalmanzar@gmail.com>
2021-07-28 14:35:56 +0530

    Initial commit

--- tweetme/settings.py
+++ tweetme/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'o+x7r8!s)l=e&hk!hb*uc!)8g)pv=#sg7do59823d#_f$k5v96'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
--- tweets/views.py
+++ tweets/views.py
@@ -1,13 +1,17 @@
+from tweetme.settings import ALLOWED_HOSTS
 from django.http.response import Http404, JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.utils.http import is_safe_url
+from django.conf import settings
 import random
 
 from .models import Tweet
 from .forms import TweetForm
 
-# Create your views here.
+ALLOWED_HOSTS = settings.ALLOWED_HOSTS
 
+# Create your views here.
 
 def home_view(request, *args, **kwargs):
     return render(request, "pages/home.html")
@@ -15,11 +19,19 @@ def home_view(request, *args, **kwargs):
 
 def tweet_create_view(request, *args, **kwargs):
     form = TweetForm(request.POST or None)
+    next_url = request.POST.get('next') or None
     if form.is_valid():
         obj = form.save(commit=False)
         # do other form logic
         obj.save()
+        if request.is_ajax():
+            return JsonResponse(obj.serialize(), status=201)
+        if next_url != None and is_safe_url(next_url, ALLOWED_HOSTS):
+            return redirect(next_url)
         form = TweetForm()
+    if form.errors:
+        if request.is_ajax():
+            return JsonResponse(form.errors, status=400)
 
     return render(request, "components/form.html", context={"form": form})
 
@@ -32,8 +44,7 @@ def tweet_list_view(request, *args, **kwargs):
     return json data
     """
     qs = Tweet.objects.all()
-    tweets_list = [{"id": x.id, "content": x.content,
-                    "likes": random.randint(0, 145)} for x in qs]
+    tweets_list = [x.serialize() for x in qs]
     data = {
         "isUser": False,
         "response": tweets_list
